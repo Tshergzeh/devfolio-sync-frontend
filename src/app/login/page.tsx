7cbec7b27862e75717d2dbbda2c699903e4414b1
Oluwasegun Ige <mailshegze@gmail.com>
2025-10-31 14:11:23 +0100

    implement dark mode

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/login-form";
+import { ThemeToggle } from "@/components/theme-toggle";
+
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <ThemeToggle />
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
